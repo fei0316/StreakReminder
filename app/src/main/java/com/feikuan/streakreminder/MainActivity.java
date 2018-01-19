cf8d115e94144fcdf6bacced9e8e7ecc59988e35
fei <fei0316@users.noreply.github.com>
2018-01-19 22:00:17 +0800

    Snackbar fixed, layout redesigned, color issue fixed

--- app/src/main/java/com/feikuan/streakreminder/MainActivity.java
+++ app/src/main/java/com/feikuan/streakreminder/MainActivity.java
@@ -1,23 +1,24 @@
 package com.feikuan.streakreminder;
 
-import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.PendingIntent;
-import android.content.Context;
+//import android.app.AlarmManager;
+//import android.app.Notification;
+//import android.app.PendingIntent;
+//import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.SystemClock;
+//import android.graphics.Color;
+//import android.os.SystemClock;
 import android.preference.PreferenceManager;
-import android.support.constraint.ConstraintLayout;
+//import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.Button;
+//import android.widget.RelativeLayout;
 import android.widget.TextView;
+//import java.util.Locale;
 
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,54 +34,59 @@ public class MainActivity extends AppCompatActivity {
         Button openSnap = findViewById(R.id.button3);
         openSnap.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                LaunchSnap();
+                LaunchSnap(v);
             }
         });
-            LoadConfig();
-        }
+        LoadConfig();
+    }
 
     public void onResume() {
         super.onResume();
         LoadConfig();
     }
 
-    public void LoadConfig () {
+    public void LoadConfig() {
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
         long time_last = settings.getLong("lastsnaptime", 0);
         LoadCurrentTime(time_last);
     }
 
-    public void LoadCurrentTime (long time_last) {
+    public void LoadCurrentTime(long time_last) {
         long millis_now = System.currentTimeMillis();
-        long time_till = millis_now - time_last ;
+        long time_till = millis_now - time_last;
         String showTime;
         int showHours, showMin, showSec;
-        if (time_till>604800000) {
-            showTime = "Long ago...";
-        }
-        else {
+        if (time_till > 604800000) {
+            showTime = getString(R.string.long_ago);
+        } else {
             long longHours = (time_till / 1000 / 60 / 60);
             long longMin = (time_till / 1000 / 60 % 60);
             long longSec = (time_till / 1000 % 60);
             showHours = (int) longHours;
             showMin = (int) longMin;
             showSec = (int) longSec;
-            showTime = String.format("%02d", showHours)+":"+String.format("%02d", showMin)+":"+String.format("%02d", showSec);
+            showTime = String.format("%02d", showHours) + ":" + String.format("%02d", showMin) + ":" + String.format("%02d", showSec);
+            // terrible way to convert TimeMillis to hh:mm:ss notation, does not take account into other ways of time formats and may be inefficient
         }
         TextView lastSnapTime = findViewById(R.id.textView4);
 
-       if (time_till > 43200000 && time_till < 72000000) {
-           lastSnapTime.setTextColor(getResources().getColor(R.color.colorOrange));
-       }
+        if (time_till>43200000 && time_till<72000000) {
+            lastSnapTime.setTextColor(getResources().getColor(R.color.colorOrange));
+        }
 
-       if (time_till>72000000) {
-           lastSnapTime.setTextColor(getResources().getColor(R.color.colorRed));
-       }
+        if (time_till>72000000) {
+            lastSnapTime.setTextColor(getResources().getColor(R.color.colorRed));
+        }
 
-       if (time_till<0) {
-           showTime = "Corrupted Time! Clear Data!";
-           lastSnapTime.setTextColor(getResources().getColor(R.color.colorRed));
-       }
+        if (time_till<0) {
+            showTime = getString(R.string.corrupted_data);
+            lastSnapTime.setTextColor(getResources().getColor(R.color.colorRed));
+        }
+
+        if (time_till>=0 && time_till<=4320000) {
+            lastSnapTime.setTextColor(getResources().getColor(R.color.colorBlack));
+        }
+
         lastSnapTime.setText(showTime);
 
 //       scheduleNotification();
@@ -101,21 +107,21 @@ public class MainActivity extends AppCompatActivity {
 
 //      inconplete implementation of notification
 
-    private void SaveTime () {
+    public void SaveTime() {
+        long saveLongTime = System.currentTimeMillis();
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = pref.edit();
-        editor.putLong("lastsnaptime", System.currentTimeMillis());
+        editor.putLong("lastsnaptime", saveLongTime);
         editor.apply();
         LoadConfig();
     }
 
-    private void LaunchSnap () {
-        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.snapchat.android");
+    public void LaunchSnap(View v) {
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage(getString(R.string.snapchat_package));
         if (launchIntent != null) {
             startActivity(launchIntent);//null pointer check in case package name was not found
-        }
-            /*else {
-                Snackbar.make(findViewById(android.R.id.content), "Snapchat not installed", Snackbar.LENGTH_SHORT).show;
-            }*/
+        } else {
+            Snackbar.make(v, R.string.not_installed, Snackbar.LENGTH_SHORT).show();
         }
     }
+}
